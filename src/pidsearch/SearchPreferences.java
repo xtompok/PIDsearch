899564e13910d4dfef16b5ea7378a9aa6a0636d1
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2013-02-17 22:01:09 +0100

    Added SearchPreferences.

---
+++ src/pidsearch/SearchPreferences.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pidsearch;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author jethro
+ */
+public class SearchPreferences {
+    Vertex from;
+    Vertex to;
+    Calendar when;
+    boolean quiet;
+    
+}
